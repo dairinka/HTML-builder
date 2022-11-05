1c02ca3ee9395d713ff0a1a152772a44dad9d9e8
dairinka <104695300+dairinka@users.noreply.github.com>
2022-11-05 22:57:10 +0200

    feat: add solution for 3 task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const fs = require('fs');
+const { stdout } = process;
+
+fs.readdir(path.join(__dirname, 'secret-folder'), 'utf-8', (err, files) => {
+     
+  if (err) throw err;
+    
+  files.forEach(file => {
+    
+    fs.stat(path.join(__dirname, 'secret-folder', file), (err, stats) => {
+          
+      if (err) throw err;
+         
+      if (stats.isFile()) {
+        let onlyFileName = file.split(".")[0];
+        let fileExtension = file.split(".")[1];
+        stdout.write(`${onlyFileName} - ${fileExtension} - ${stats.size / 1000}kb\n`);
+      }
+    });
+  })
+    
+} )
