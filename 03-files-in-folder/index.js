333f90fa2de23bada8871c2d343046fe9521b753
dairinka <104695300+dairinka@users.noreply.github.com>
2022-11-08 10:30:39 +0200

    fix: correct conversion to kb in task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -15,7 +15,7 @@ fs.readdir(path.join(__dirname, 'secret-folder'), 'utf-8', (err, files) => {
       if (stats.isFile()) {
         let onlyFileName = file.split(".")[0];
         let fileExtension = file.split(".")[1];
-        stdout.write(`${onlyFileName} - ${fileExtension} - ${stats.size / 1000}kb\n`);
+        stdout.write(`${onlyFileName} - ${fileExtension} - ${stats.size / 1024}kb\n`);
       }
     });
   })
